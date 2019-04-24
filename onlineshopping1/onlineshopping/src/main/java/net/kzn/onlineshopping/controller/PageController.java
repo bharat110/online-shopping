4b6e77c19f72cb2bb7a8586177a5eee7bc5bad84
Lalji Rathod <lalji.rathod27@gmail.com>
2019-04-24 19:15:01 +0530

    Complete User Interface and theme and other issues..

--- onlineshopping1/onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping1/onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,39 +1,45 @@
 package net.kzn.onlineshopping.controller;
 
-import javax.ws.rs.PathParam;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 
-	@RequestMapping(value = { "/", "/home", "/index" })
+	@RequestMapping(value = { "/", "/home" })
 	public ModelAndView index() {
-		ModelAndView mv = new ModelAndView("page");
-		// passing the list of categories
-		mv.addObject("greeting", "hello");
-
-		return mv;
-	}
-
-	@RequestMapping(value = "/greeting")
-	public ModelAndView test(@RequestParam("greeting") String greeting) {
-
 		ModelAndView mav = new ModelAndView("page");
-		mav.addObject("greeting", greeting);
+		// passing the list of categories
+		mav.addObject("title", "Home");
+		mav.addObject("userClickHome", true);
+
 		return mav;
 	}
 	
-	@RequestMapping(value = "/test/{test}")
-	public ModelAndView pathVariable(@PathVariable("test") String test) {
-
+	@RequestMapping(value ="/about")
+	public ModelAndView about() {
 		ModelAndView mav = new ModelAndView("page");
-		mav.addObject("test", test);
+		// passing the list of categories
+		mav.addObject("title", "About Us");
+		mav.addObject("userClickAbout", true);
+
 		return mav;
 	}
+
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mav = new ModelAndView("page");
+		// passing the list of categories
+		mav.addObject("title", "Contact Us");
+		mav.addObject("userClickContact", true);
+
+		return mav;
+	}
+
+
+	
 
 }
